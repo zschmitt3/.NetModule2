e9a41356fd99bb4e7f75a3c95602308d3d4a4e1c
zschmitt3 <zschmitt3@my.wctc.edu>
2023-02-06 13:43:28 -0600

    V1.1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Console.WriteLine("Enter 1 to create data file.");
+Console.WriteLine("Enter 2 to parse data.");
+Console.WriteLine("Enter anything else to quit.");
+// input response
+string? resp = Console.ReadLine();
+
+if (resp == "1")
+{
+    // TODO: create data file
+}
+else if (resp == "2")
+{
+    // TODO: parse data file
+
+}
