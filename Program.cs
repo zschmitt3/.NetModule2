bb12989416e889d159f107906c3213fc70ba3c1c
zschmitt3 <zschmitt3@my.wctc.edu>
2023-02-06 14:12:17 -0600

    Homework Start

--- Program.cs
+++ Program.cs
@@ -1,26 +1,37 @@
 ﻿Console.WriteLine("Enter 1 to create data file.");
 Console.WriteLine("Enter 2 to parse data.");
 Console.WriteLine("Enter anything else to quit.");
-// input response
 string? resp = Console.ReadLine();
 
 if (resp == "1")
 {
 
     Console.WriteLine("How many weeks of data is needed?");
-    // input the response (convert to int)
     int weeks = int.Parse(Console.ReadLine());
     
-    // determine start and end date
     DateTime today = DateTime.Now;
-    // we want full weeks sunday - saturday
     DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
-    // subtract # of weeks from endDate to get startDate
     DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
-    Console.WriteLine(dataDate);
+    
+    Random rnd = new Random();
+    StreamWriter sw = new StreamWriter("data.txt");
+
+
+    while (dataDate < dataEndDate)
+    {
+        int[] hours = new int[7];
+        for (int i = 0; i < hours.Length; i++)
+        {
+            hours[i] = rnd.Next(4, 13);
+        }
+        sw.WriteLine($"{dataDate:M/d/yyyy},{string.Join("|", hours)}");
+        dataDate = dataDate.AddDays(7);
+    }
+    sw.Close();
 }
 else if (resp == "2")
 {
-    // TODO: parse data file
+    StreamReader sr = new StreamReader("data.txt");
+    
 
 }
